265e45f0d6119f15dda85b8a23e5afa0d98e0b1d
Maxime Petit <contactmaximepetit@gmail.com>
2023-02-13 01:26:10 +0100

    [ADD] init map

---
+++ src/components/Map/map.js
@@ -0,0 +1,28 @@
+import Map, {
+  NavigationControl,
+  FullscreenControl,
+  ScaleControl,
+  GeolocateControl,
+} from "react-map-gl";
+
+export default function InteractiveMap({}) {
+  return (
+    <Map
+      initialViewState={{
+        longitude: 2,
+        latitude: 47,
+        zoom: 5,
+        bearing: 0,
+        pitch: 0
+      }}
+      style={{ width: "100vw", height: "100vh" }}
+      mapStyle="mapbox://styles/mapbox/light-v11"
+      mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN}
+    >
+      <GeolocateControl position="top-left" />
+      <FullscreenControl position="top-left" />
+      <NavigationControl position="top-left" />
+      <ScaleControl />
+    </Map>
+  );
+}
