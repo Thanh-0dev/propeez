9ac02238d91ea7a77f23d4facc31cf9689f3f74e
Maxime Petit <contactmaximepetit@gmail.com>
2023-02-13 22:34:18 +0100

    [ADD] style of popup container with default img

--- src/components/Map/PopupBox/popupBox.js
+++ src/components/Map/PopupBox/popupBox.js
@@ -1,21 +1,35 @@
+import Image from "next/image";
 import { Popup } from "react-map-gl";
+import styles from "./popupBox.module.css";
 
 export default function PopupBox({ setPopupInfo, popupInfo }) {
+  console.log(popupInfo.image);
   return (
     <Popup
+      className={styles.popupContainer}
       anchor="top"
       longitude={Number(popupInfo.longitude)}
       latitude={Number(popupInfo.latitude)}
       onClose={() => setPopupInfo(null)}
     >
-      <div>
-        <h2>{popupInfo.title}</h2>
-        <p>{popupInfo.adress}</p>
-        <p>{popupInfo.description}</p>
-        <p>
-          Du {popupInfo.startDate} au {popupInfo.endDate}
-        </p>
-      </div>
+      <Image
+        src="/default.jpg"
+        alt={popupInfo.title}
+        className={styles.image}
+        width={210}
+        height={120}
+      />
+      <h2 className={styles.title}>{popupInfo.title}</h2>
+      <p className={styles.item}>
+        <strong>Lieu</strong> : {popupInfo.address}
+      </p>
+      <p className={styles.item}>
+        <strong>Description</strong> : {popupInfo.description}
+      </p>
+      <p className={styles.item}>
+        Du <strong>{popupInfo.startDate}</strong> au{" "}
+        <strong>{popupInfo.endDate}</strong>
+      </p>
     </Popup>
   );
 }
