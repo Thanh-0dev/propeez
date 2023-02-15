4b64a3913d3b1c7d31f8b34d3352c3bba68d6e4c
Thanh-Long Pham <thanh@theoffline.fr>
2023-02-15 10:27:16 +0100

    [EDIT] Mapbox style

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,19 @@
-import "../globals.css";
-import Head from "next/head";
-import Layout from "@/components/Layout/layout";
+import '../globals.css';
+import Head from 'next/head';
+import { SessionProvider } from 'next-auth/react';
 
-export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          href="https://api.mapbox.com/mapbox-gl-js/v2.6.1/mapbox-gl.css"
-          rel="stylesheet"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+export default function App({ Component, pageProps, session }) {
+	return (
+		<SessionProvider session={session}>
+			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+				<link
+					href="https://api.mapbox.com/mapbox-gl-js/v2.6.1/mapbox-gl.css"
+					rel="stylesheet"
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</SessionProvider>
+	);
 }
