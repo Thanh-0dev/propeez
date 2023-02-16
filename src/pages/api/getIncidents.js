dd94deb24670154d46a726ee88fe1d2114134832
Thanh-Long Pham <thanh@theoffline.fr>
2023-02-16 15:21:13 +0100

    [ADD] GetIncidents

---
+++ src/pages/api/getIncidents.js
@@ -0,0 +1,22 @@
+export default async function handle(req, res) {
+	const { latitude, longitude, category } = req.body;
+	const radius = 30;
+
+	const query =
+		await prisma.$queryRaw`SELECT id FROM "Incident" WHERE ST_DWithin(ST_MakePoint(longitude, latitude), ST_MakePoint(${longitude}, ${latitude})::geography, ${radius} * 1000)`;
+	const incidents = await prisma.incident.findMany({
+		where: {
+			id: {
+				in: query.map(({ id }) => id),
+			},
+			published: true,
+			...(category ? { category: { type: category } } : {}),
+		},
+		include: {
+			image: true,
+			category: true,
+		},
+	});
+
+	res.json(incidents);
+}
