0f44143a6f850aad23d990fcef0ad6dd738a3765
Thanh-Long Pham <thanh@theoffline.fr>
2023-02-17 15:08:23 +0100

    [EDIT] Fix category color

--- src/components/Admin/Category/Category.jsx
+++ src/components/Admin/Category/Category.jsx
@@ -2,7 +2,14 @@ import styles from './Category.module.css';
 
 export default function Category({ category }) {
 	return (
-		<div className={styles.category} style={{ background: category.color }}>
+		<div
+			className={styles.category}
+			style={
+				category
+					? { backgroundColor: category.color }
+					: { backgroundColor: '#000' }
+			}
+		>
 			<p>{category.type}</p>
 		</div>
 	);
